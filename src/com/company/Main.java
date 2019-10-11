35a94035f50feb6108f436a64eca565d4fa35be8
Krystian Sosnowski <kr.sosnowski@gmail.com>
2019-10-11 22:37:20 +0200

    Nowy Branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int[] myIntArray = new int[25];//{1, 2, 3,4,5,6,7,8,9,10};
+	    int[] myIntArray = new int[24];//{1, 2, 3,4,5,6,7,8,9,10};
 //	    myIntArray[0]=45;
 //	    myIntArray[1]=476;
 //	    myIntArray[5]=50;
